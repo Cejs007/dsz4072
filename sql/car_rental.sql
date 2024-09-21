36389194c4d697e57b7ff89b18c5e95f8d9bdf9a
Cejs007 <michal.maliszewski2@gmail.com>
2024-09-21 13:13:57 +0200

    added task 4

--- sql/car_rental.sql
+++ sql/car_rental.sql
@@ -62,3 +62,54 @@ delete from clients where client_id=1;
 
 insert into clients (first_name, last_name, address, city) values ('Miloš', 'Zeman', 'Vysočina', 'Les');
 insert into clients (first_name, last_name, address, city) values ('Petr', 'Pavel', 'Pražský hrad', 'Praha');
+
+delete from bookings where booking_id > 0;
+alter table bookings AUTO_INCREMENT = 1;
+delete from clients where client_id > 0;
+alter table clients AUTO_INCREMENT = 1;
+delete from cars where car_id > 0;
+alter table cars AUTO_INCREMENT = 1;
+
+INSERT INTO clients (first_name, last_name, address, city) VALUES
+ ('Michal', 'Taki', 'os. Srodkowe 12', 'Poznan'),
+ ('Pawel', 'Ktory', 'ul. Stara 11', 'Gdynia'),
+ ('Anna', 'Inna', 'os. Srednie 1', 'Gniezno'),
+ ('Alicja', 'Panna', 'os. Duze 33', 'Torun'),
+ ('Damian', 'Papa', 'ul. Skosna 66', 'Warszawa'),
+ ('Marek', 'Troska', 'os. Male 90', 'Radom'),
+ ('Jakub', 'Klos', 'os. Polskie 19', 'Wadowice'),
+ ('Lukasz', 'Lis', 'os. Podlaskie 90', 'Bialystok');
+ 
+INSERT INTO cars (manufacturer, model, year, horse_power, price_per_day) VALUES
+ ('Mercedes', 'CLK', 2018, 190, 400),
+ ('Hyundai', 'Coupe', 2014, 165, 300),
+ ('Dacia', 'Logan', 2015, 103, 150),
+ ('Saab', '95', 2012, 140, 140),
+ ('BMW', 'E36', 2007, 110, 80),
+ ('Fiat', 'Panda', 2016, 77, 190),
+ ('Honda', 'Civic', 2019, 130, 360),
+ ('Volvo', 'XC70', 2013, 180, 280);
+ 
+ INSERT INTO bookings (client_id, car_id, start_date,
+end_date, total_amount) VALUES
+ (3, 3, '2020-07-06', '2020-07-08', 400),
+ (6, 1, '2020-07-10', '2020-07-16', 1680),
+ (4, 5, '2020-07-11', '2020-07-14', 450),
+ (5, 4, '2020-07-11', '2020-07-13', 600),
+ (7, 3, '2020-07-12', '2020-07-14', 800),
+ (5, 7, '2020-07-14', '2020-07-17', 240),
+ (3, 8, '2020-07-14', '2020-07-16', 380),
+ (5, 2, '2020-07-15', '2020-07-18', 1080),
+ (6, 3, '2020-07-16', '2020-07-20', 1120),
+ (8, 1, '2020-07-16', '2020-07-19', 600),
+ (1, 2, '2020-07-16', '2020-07-21', 500),
+ (2, 6, '2020-07-17', '2020-07-19', 280),
+ (1, 4, '2020-07-17', '2020-07-19', 720),
+ (3, 7, '2020-07-18', '2020-07-21', 240),
+ (5, 4, '2020-07-18', '2020-07-22', 1200);
+
+select * from cars where year > 2015;
+select * from bookings where total_amount between 1000 and 2555;
+select client_id from clients where first_name like 'A%' and last_name like '%a';
+
+select * from clients where client_id in (3, 4);
